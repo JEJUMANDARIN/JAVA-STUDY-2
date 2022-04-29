7d1f35e06ea9f52d6a4d7e359a8da9a44ece686e
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-29 21:58:47 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/LineComment.java
@@ -0,0 +1,18 @@
+//   파일이름: BlockComment.java
+//  작성자: HIYOKO
+//  작성일: 2022.04.29
+//  목적: System.out.println 메소드의 기능 테스트
+
+public class LineComment {
+    public static void main(String[] args)
+    {
+        System.out.println(7); //다음은 단순한 정수의 출력
+
+        System.out.println(3.15);
+        System.out.println("3 + 5 = " + 8);
+        System.out.println(3.15 + "는 실수입니다.");
+        System.out.println("3 + 5" + "의 연산 결과는 8입니다.");
+
+        System.out.println(3+5);    // 덧셈 결과의 출력
+    }
+}
