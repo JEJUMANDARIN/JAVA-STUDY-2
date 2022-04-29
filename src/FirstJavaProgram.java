aaef2ad38aead6ca8b2f12658a57e12d66272b54
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-29 20:03:34 +0900

    01

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java");
+        System.out.println("First Java program");
+    }
+}
