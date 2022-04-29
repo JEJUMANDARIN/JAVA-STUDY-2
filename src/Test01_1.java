7a10813d0ba6a9f066f0e9feba70e73cb96e61ef
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-29 21:24:03 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/Test01_1.java
@@ -0,0 +1,20 @@
+public class Test01_1 {
+    /*
+     * 1.문제
+     * FirstJavaProgram.java를 컴파일하면 FirstJavaProgram.class가 생성된다.
+     * 그런데 컴파일 시 생성되는 클래스 파일의 이름을 변경하고 싶다.
+     * 예를 들어 SimpleJavaProgram.class가 생성되게 하려면 무엇을 수정해야 되겠는가?
+     * 나름대로 예측해보자
+     *
+     * 1.답변
+     * 클래스의 이름을 SimpleJavaProgram으로 수정한다.
+     */
+
+    public static void main(String[] args)
+    {
+        System.out.println("2 + 5 = " + 2 + 5 );
+        System.out.println("2 + 5 = " + (2 + 5));
+    }
+}
+
+
