ee73803299dfb1fb30790fd7ff52760b1ebca1ec
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-30 16:57:26 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/OperatePromotion.java
@@ -0,0 +1,9 @@
+public class OperatePromotion {
+    public static void main(String[] args)
+    {
+        int num1 = 11;
+        int num2 = 22;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
