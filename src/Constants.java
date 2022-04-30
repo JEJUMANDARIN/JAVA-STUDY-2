ed73d46a5e2a9e3be60bc2f672c988b4bffa246b
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-04-30 19:43:47 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+    public static void main(String[] args)
+    {
+        final int MAX_SIZE = 100;   //상수 MAX_SIZE의 선언
+        final char CONST_CHAR = '상';
+        final int CONST_ASSIGNED;
+
+        CONST_ASSIGNED = 12;    //할당하지 않았던 상수의 값 할당
+        System.out.println("상수1 : " + MAX_SIZE);
+        System.out.println("상수2 : " + CONST_CHAR);
+        System.out.println("상수3 : " + CONST_ASSIGNED);
+    }
+}
