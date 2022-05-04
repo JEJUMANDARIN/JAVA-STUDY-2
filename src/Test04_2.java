f62e2bb9e5ee92017e97ee4f647aead96501df79
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-05-04 15:22:29 +0900

    윤성우의 열혈 Java 프로그래밍

--- src/Test04_2.java
+++ src/Test04_2.java
@@ -11,7 +11,7 @@ public class Test04_2 {
 
         num1 += 10;
         num2 += 10;
-        result= (num1 < 0) && (num2 > 0);
+        result = (num1 < 0) && (num2 > 0);
 
         System.out.println("result = " + result);
         System.out.println("num1 = " + num1);
