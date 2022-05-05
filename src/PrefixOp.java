d80564d0fdb2939352a46f2b11ff4c730f729c76
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-05-05 20:22:28 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/PrefixOp.java
@@ -0,0 +1,9 @@
+public class PrefixOp {
+    public static void main(String[] args)
+    {
+        int num = 7;
+        System.out.println(++num);  // num의 값 하나 증가 후 출력
+        System.out.println(++num);  // num의 값 하나 증가 후 출력
+        System.out.println(num);
+    }
+}
