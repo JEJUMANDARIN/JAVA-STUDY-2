1a20b4d71b769147dcc050d5fe42a6fc212f32e7
JEJUMANDARIN <jejutangerinee@gmail.com>
2022-05-05 21:10:43 +0900

    윤성우의 열혈 Java 프로그래밍

---
+++ src/IEBasic.java
@@ -0,0 +1,22 @@
+public class IEBasic {
+    public static void main(String[] args)
+    {
+        int n1 = 5;
+        int n2 = 7;
+
+        // if문
+        if(n1 < n2){
+            System.out.println("n1 < n2 is true");
+        }
+
+        // if ~ else문
+        if(n1 == n2){
+            System.out.println("n1 == n2 is true");
+        }
+
+        //else문
+        else {
+            System.out.println("n1 == n2 is false");
+        }
+    }
+}
